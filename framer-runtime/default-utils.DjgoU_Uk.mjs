f08740ee986039bf5a3b55c4d09a7da44bf185f7
tefitei <stefaniwork120silva@gmail.com>
2026-06-02 23:57:21 -0300

    Delete framer-runtime directory

--- framer-runtime/default-utils.DjgoU_Uk.mjs
+++
@@ -1,2 +0,0 @@
-﻿import{t as e}from"./rolldown-runtime.BvHXOKAc.mjs";import{t}from"./OIjZRBmWDcIE2B6qgG1j.uVSxg68N.mjs";var n=e((()=>{t()}));export{n as t};
-//# sourceMappingURL=default-utils.DjgoU_Uk.mjs.map
